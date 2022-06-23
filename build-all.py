5fad5178207bfdd4e644f69a4f96c7414a1df751
Dave Tonge <dave.tonge@moneyhub.com>
2022-06-23 19:09:36 +0000

    Adjust build script

--- build-all.py
+++ build-all.py
@@ -21,6 +21,7 @@ dirs_exclude = [
 # Some of them we likely don't care about, but some of them should be fixed and removed from the list
 files_exclude = [
     './FAPI_2_0_Advanced_Authorization_Profile.md',
+    './FAPI_2_0_Baseline_Profile.md',
     './Financial_API_Lodging_Intent.md',
     './Financial_API_Pushed_Request_Object.md',
     './Financial_API_Simple_HTTP_Message_Integrity_Protocol.md',
