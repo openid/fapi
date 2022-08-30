bfcfaacff6db74ea9d79845e9c1f8b255924d279
Joseph Heenan <joseph@authlete.com>
2022-08-30 18:15:41 +0900

    Add place holder so links to JARM spec in old location aren't 404s
    commit d5aa104e renamed the file.

--- build-all.py
+++ build-all.py
@@ -23,6 +23,7 @@ files_exclude = [
      './FAPI_2_0_Advanced_Profile.md',
     './FAPI_2_0_Advanced_Authorization_Profile.md',
     './FAPI_2_0_Baseline_Profile.md',
+    './Financial_API_JWT_Secured_Authorization_Response_Mode.md',
     './Financial_API_Lodging_Intent.md',
     './Financial_API_Pushed_Request_Object.md',
     './Financial_API_Simple_HTTP_Message_Integrity_Protocol.md',
